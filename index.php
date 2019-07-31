f894b72a55b48842baa224a7c90e2775b54d8bb0
Jeorge-1831 <53499795+Jeorge-1831@users.noreply.github.com>
2019-07-31 10:00:14 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head><title>Profile</title></head>
+<header>
+<center><h1>Jeorge Ronnel Abangan</h1></center>
+</header>
+<body style="border: solid;">
+<main>
+<ul>
+<li>17 years ol</li>
+<li>City Of Naga, Cebu</li>
+<li>BSIT-1 UJSR</li>
+
+<footer>
+	<p>Created by: Jeorge Ronnel Abangan</p>
+</footer>
+</ul>
+</main>
+</body>
+<html>
